ce98f2959a8365e65bf0424d63a65dc800a0505f
Daniel Williams <dlw1ll27@gmail.com>
2021-04-05 19:11:02 -0700

    adds routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,31 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/exercises" => "photos#index"
+    #exercises
+    get "/exercises", controller: "exercises", action: "index"
+    get "/exercises/:id", controller: "exercises", action: "show"
+    post "/exercises", controller: "exercises", action: "create"
+    patch "/exercises/:id", controller: "exercises", action: "update"
+    delete "/exercises/:id", controller: "exercises", action: "destroy"
+
+    #workouts
+    get "/workouts", controller: "workouts", action: "index"
+    get "/workouts/:id", controller: "workouts", action: "show"
+    post "/workouts", controller: "workouts", action: "create"
+    patch "/workouts/:id", controller: "workouts", action: "update"
+    delete "/workouts/:id", controller: "workouts", action: "destroy"
+
+    #workout_exercises
+    get "/workout_exercises", controller: "workout_exercises", action: "index"
+    get "/workout_exercises/:id", controller: "workout_exercises", action: "show"
+    post "/workout_exercises", controller: "workout_exercises", action: "create"
+    patch "/workout_exercises/:id", controller: "workout_exercises", action: "update"
+    delete "/workout_exercises/:id", controller: "workout_exercises", action: "destroy"
+
+    #user
+    post "/users", controller: "users", action: "create"
+
+    #sessions
+    post "/sessions", controller: "sessions", action: "create"
   end
 end
